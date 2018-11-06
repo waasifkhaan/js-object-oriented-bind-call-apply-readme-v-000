a2c10db6e51ec746d7d655e322d00439389efe7d
Wasif Khan <waasif.khaan@gmail.com>
2018-11-06 00:25:06 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // use this to copy code snippets or us your browser's console
+class Event {
+    constructor(title, keywords) {
+        this.title = title;
+        this.keywords = keywords;
+    }
+}
+
+class User {
+    constructor(name, interests) {
+        this.name = name;
+        this.interests = interests;
+    }
+
+    matchInterests(event) {
+        return event.keywords.some(function(word) {
+            return this.interests.includes(word);
+        });
+    }
+}
+
+let billy = new User('billy', ['music', 'art', 'movies']);
+let freeMusic = new Event('Free Music Show', ['music', 'free', 'outside']);
+
+billy.matchInterests(freeMusic);
